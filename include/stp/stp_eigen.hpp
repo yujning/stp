2116a152e86a9cc889c0fa3ff60541995a6d68c0
zfchu <chuzhufei@nbu.edu.cn>
2023-10-10 10:55:25 +0800

    print

--- include/stp/stp_eigen.hpp
+++ include/stp/stp_eigen.hpp
@@ -5,25 +5,9 @@
 #include <iostream> 
 #include <numeric>
 
-using matrix = Eigen::MatrixXi;      // Defines the type of matrix to use
+using matrix = Eigen::MatrixXi;           // Defines the type of matrix to use
 using matrix_chain = std::vector<matrix>; // Defined matrix chain
 
-/*
-  Some static global functions for accessibility.
-  Its scope is in this folder only and is used to implement encapsulation.
-*/
-
-/* Obtain the least common multiple of m and n. */
-//static unsigned get_lcm( unsigned m, unsigned n );
-
-/* Identify whether a matrix is representing a variable. */
-//static bool is_variable( const matrix& mat );
-
-/* If a matrix represents a variable, return the variable, otherwise return 0. */
-//static int get_variable( const matrix& mat );
-
-/* String segmentation function, can achieve multiple characters as delimiters to partition. */
-//static std::vector<std::string> m_split( const std::string& input, const std::string& pred );
 
 namespace stp
 {
@@ -47,7 +31,6 @@ std::vector<std::string> matrix_split( const std::string& input, const std::stri
       }
     }
     
-    // if input[i] != pred中的任何一个 该字符加到temp上
     if ( j == count2 )
     {
       temp += input[i];
@@ -83,11 +66,29 @@ bool is_variable( const matrix& mat )
 
 int get_variable( const matrix& mat )
 {
-  if ( mat.rows() == 2 && mat.cols() == 1 )
+  if( is_variable( mat ) )
   {
     return mat( 0, 0 );
   }
-  return 0;
+  else
+  {
+    return 0;
+  }
+}
+
+void print_binary( const matrix& mat, std::ostream& os = std::cout )
+{
+  for( auto i = mat.cols() - 1; i >= 0; i-- )
+  {
+    os << mat( 0, i );
+  }
+}
+
+std::string to_binary( const matrix& mat )
+{
+  std::stringstream st;
+  print_binary( mat, st );
+  return st.str();
 }
 
 matrix generate_swap_matrix( const int& m, const int& n )
@@ -175,31 +176,41 @@ matrix normalize_matrix( matrix_chain mc )
 {
   matrix Mr( 4, 2 ); // Reduced power matrix
   Mr << 1, 0, 0, 0, 0, 0, 0, 1;
+
   matrix I2( 2, 2 ); // Identity matrix
   I2 << 1, 0, 0, 1;
+  
   matrix normal_matrix;
   int p_variable;
   int p;
+
   int max = 0; // the max is the number of variable
+  
   for ( int i = 0; i < mc.size(); i++ )
   {
     if ( mc[i]( 0, 0 ) > max )
+    {
       max = mc[i]( 0, 0 );
+    }
   }
+
   std::vector<int> idx( max + 1 ); // id[0] is the max of idx
   p_variable = mc.size() - 1;
+
   while ( p_variable >= 0 )
   {
     bool find_variable = false;
     matrix& matrix = mc[p_variable];
     int var = get_variable( matrix );
+    
     if ( var != 0 ) // 1:find a variable
     {
       if ( idx[var] == 0 ) // the variable appears for the first time ：end : not_end
       {
         idx[var] = idx[0] + 1;
         idx[0]++;
-        if ( p_variable == mc.size() - 1 ) // 第一次出现的变量在矩阵链尾部
+
+        if ( p_variable == mc.size() - 1 ) // the variable shows in the end
         {
           mc.pop_back();
           p_variable--;
@@ -216,11 +227,13 @@ matrix normalize_matrix( matrix_chain mc )
         {
           find_variable = true;
           mc.push_back( generate_swap_matrix( 2, 1 << ( idx[0] - idx[var] ) ) );
+          
           for ( int i = 1; i <= max; i++ )
           {
             if ( idx[i] != 0 && idx[i] > idx[var] )
               idx[i]--;
           }
+
           idx[var] = idx[0];
         }
       }
@@ -251,7 +264,9 @@ matrix normalize_matrix( matrix_chain mc )
       }
 
       if ( find_variable )
+      {
         mc.push_back( Mr );
+      }
       continue;
     }
     else
@@ -260,46 +275,62 @@ matrix normalize_matrix( matrix_chain mc )
     }
   }
   
-  for ( int i = max; i > 0; i-- ) //!
+  for ( int i = max; i > 0; i-- ) 
   {
     mc.push_back( generate_swap_matrix( 2, pow( 2, idx[0] - idx[i] ) ) );
+
     for ( int j = 1; j <= max; j++ )
     {
-      if ( idx[j] != 0 && idx[j] > idx[i] )
+      if ( ( idx[j] != 0 ) && ( idx[j] > idx[i] ) )
+      {
         idx[j]--;
+      }
     }
+
     idx[i] = max;
   }
+
   normal_matrix = matrix_chain_multiply( mc );
   return normal_matrix;
 }
 
-// 表达式字符串   +   给定排序的变量名字   +    是否需要打印生成的矩阵链
-// example:             (a & b) | (a & ~c) | (~b & ~c)                                 a b c
-matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::string>& input_names, bool print = false )
+// example:             (a & b) | (a & ~c) | (~b & ~c)                                 
+//                      a b c
+matrix from_exp_to_nmx( const std::string& expression, 
+                        const std::vector<std::string>& input_names, 
+                        bool print = false )
 {
-  // str -> vec<str> 区分变量和运算符
   std::vector<std::string> equation;
   equation.push_back( "(" );
   std::string temp = "";
+
   for ( int i = 0; i < expression.size(); i++ )
   {
     if ( expression[i] == ' ' )
+    {
       continue;
+    }
     else if ( expression[i] == '(' || expression[i] == ')' || expression[i] == '|' || expression[i] == '&' || expression[i] == '~' )
     {
       if ( !temp.empty() )
+      {
         equation.push_back( temp );
+      }
+
       std::string p1 = "";
       p1 = p1 + expression[i];
       equation.push_back( p1 );
       temp = "";
     }
     else
+    {
       temp = temp + expression[i];
+    }
   }
   equation.push_back( ")" );
+  
   // equation = ["(",   "(", "a", "&", "b", ")",   "(", "a", "&", "~", "c", ")",  "(", "~", "a", "&", "~", "c", ")"   ")"]
+  
   /***********/
   // deal with "~"
   for ( int i = 0; i < equation.size(); i++ )
@@ -310,12 +341,16 @@ matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::st
       equation.erase( equation.begin() + i + 1 );
     }
     else if ( equation[i] == "&" || equation[i] == "|" || equation[i] == "(" || equation[i] == ")" )
+    {
       continue;
+    }
     else
+    {
       equation[i] = "(" + equation[i] + ")";
+    }
   }
+
   // equation = ["(",   "(", "(a)", "&", "(b)", ")",   "(", "(a)", "&", "~(c)", ")",  "(", "~(a)", "&", "~(c)", ")"   ")"]
-  //?
   std::vector<int> left_bracket;
   for ( int i = 0; i < equation.size(); i++ )
   {
@@ -329,10 +364,15 @@ matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::st
       for ( int j = left_bracket[left_bracket.size() - 1] + 1; j < i; j++ ) // 遍历左右括号之间的表达式
       {
         if ( equation[j] == "|" || equation[j] == "&" )
+        {
           equ = "(" + equation[j] + ")" + equ;
+        }
         else
+        {
           equ = equ + "(" + equation[j] + ")";
+        }
       }
+
       //"(" -> equ
       equation[left_bracket[left_bracket.size() - 1]] = equ;
       equation.erase( equation.begin() + left_bracket[left_bracket.size() - 1] + 1, equation.begin() + i + 1 );
@@ -340,16 +380,21 @@ matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::st
       left_bracket.pop_back();
     }
   }
+
   std::string equ = equation[0];
   equation.clear();
   equation = matrix_split( equ, "()" );
   matrix_chain mc;
+  
   Eigen::MatrixXi Mc( 2, 4 );
   Mc << 1, 0, 0, 0, 0, 1, 1, 1;
+  
   Eigen::MatrixXi Md( 2, 4 );
   Md << 1, 1, 1, 0, 0, 0, 0, 1;
+  
   Eigen::MatrixXi Mn( 2, 2 );
   Mn << 0, 1, 1, 0;
+  
   for ( int i = 0; i < equation.size(); i++ )
   {
     if ( equation[i] == "&" )
@@ -385,7 +430,7 @@ matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::st
   // print matrix_chain
   if ( print )
   {
-    auto mat_to_var = [&]( const matrix& mat ) { return input_names[mat( 0, 0 ) - 1]; };
+    auto mat_to_var = [&]( const matrix& mat ) { return input_names[ mat( 0, 0 ) - 1 ]; };
     for ( const auto& mat : mc )
     {
       if ( is_variable( mat ) )
@@ -402,5 +447,6 @@ matrix from_exp_to_nmx( const std::string& expression, const std::vector<std::st
   matrix p = normalize_matrix( mc );
   return p;
 }
+
 } // namespace stp
 
